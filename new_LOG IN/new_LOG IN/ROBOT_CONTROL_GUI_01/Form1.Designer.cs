79ae3669cd1c911e591672309801e0cf935a1527
Le Thanh Trung <132593345+letrungdev272@users.noreply.github.com>
2023-12-30 20:38:19 +0700

    commt

--- new_LOG IN/new_LOG IN/ROBOT_CONTROL_GUI_01/Form1.Designer.cs
+++ new_LOG IN/new_LOG IN/ROBOT_CONTROL_GUI_01/Form1.Designer.cs
@@ -598,6 +598,7 @@
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
